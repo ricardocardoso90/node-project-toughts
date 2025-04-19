5cfe17727efa6f4a196d6723ccdc32e881e0c4a1
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-04-19 01:47:10 -0300

    update commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,5 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = class AuthController {
 
@@ -7,6 +9,19 @@ module.exports = class AuthController {
 
   static register(req, res) {
     res.render('auth/register');
+  };
+
+  static async registerPost(req, res) {
+    const { name, email, password, confirmpassword } = req.body;
+
+    if (password != confirmpassword) {
+      req.flash('message', 'As senhas não são iguais, tente novamente!');
+      res.render('auth/register');
+
+      return
+    };
+    
+    
   };
 
 };
