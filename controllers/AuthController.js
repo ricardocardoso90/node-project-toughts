404cd41a108ffd309685021ae14e6879ac3208db
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-04-20 20:19:54 -0300

    update commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,7 +2,6 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 
 module.exports = class AuthController {
-
   static login(req, res) {
     res.render('auth/login');
   };
@@ -20,8 +19,36 @@ module.exports = class AuthController {
 
       return
     };
-    
-    
-  };
 
+    const checkIfUsersExists = await User.findOne({ where: { email: email } });
+    if (checkIfUsersExists) {
+      req.flash('message', 'O e-mail já está em uso, tente outro!');
+      res.render('auth/register');
+
+      return
+    };
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword
+    };
+
+    try {
+      const createdUser = await User.create(user);
+
+      req.session.userid = createdUser.id;
+
+      req.flash("message", "Usuário cadastrado com sucesso!! Parabéns.");
+
+      req.session.save(() => {
+        res.redirect("/");
+      });
+    } catch (error) {
+      console.log(error);
+    };
+  };
 };
