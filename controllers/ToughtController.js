206bad7ab02d5f80bd6630bd0b028276989a306f
ricardocardoso90 <ricardomanolo90@gmail.com>
2025-04-22 22:29:47 -0300

    update commit

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,10 +2,37 @@ const User = require("../models/User");
 const Tought = require("../models/Tought");
 
 module.exports = class ToughtController {
-  
+
   // SHOWTOUGHTS
   static async showToughts(req, res) {
     res.render("toughts/home");
+  };
+
+  // EDITTOUGHT
+  static async editTought(req, res) {
+    const id = req.params.id;
+    const tought = await Tought.findOne({ where: { id: id }, raw: true });
+
+    res.render("toughts/edit", { tought });
+  };
+
+  // EDITTOUGHTPOST
+  static async editToughtPost(req, res) {
+    const id = req.body.id;
+    const tought = {
+      title: req.body.title,
+    };
+
+    try {
+      await Tought.update(tought, { where: { id: id } });
+      req.flash('message', "Pensamento atualizado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect('/toughts/dashboard');
+      });
+    } catch (error) {
+      console.log(error);
+    };
   };
 
   // CREATETOUGHTS
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -5,6 +5,8 @@ const checkAuth = require("../helpers/auth").checkAuth;
 const ToughtController = require("../controllers/ToughtController");
 
 router.get('/', ToughtController.showToughts);
+router.get('/edit/:id', checkAuth, ToughtController.editTought);
+router.post('/edit', checkAuth, ToughtController.editToughtPost);
 
 router.get('/add', checkAuth, ToughtController.createToughts);
 router.post('/add', checkAuth, ToughtController.createToughtsPost);
